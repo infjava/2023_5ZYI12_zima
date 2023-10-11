b970c530fc4d04c56862ee485a581b7174353830
Jan Janech <jan.janech@fri.uniza.sk>
2023-10-11 13:58:01 +0200

    03 - kalkulacka skopirovana z UML .FRI

---
+++ kalkulacka/kalkulacka/Kalkulacka.java
@@ -0,0 +1,31 @@
+public class Kalkulacka {
+    private double vysledok;
+    
+    public Kalkulacka() {
+        
+    }
+    
+    public void zadajCislo(double hodnota) {
+        
+    }
+    
+    public void pripocitaj(double hodnota) {
+        
+    }
+    
+    public void odpocitaj(double hodnota) {
+        
+    }
+    
+    public void vynasob(double hodnota) {
+        
+    }
+    
+    public void vydel(double hodnota) {
+        
+    }
+    
+    public double getVysledok() {
+        return this.vysledok;
+    }
+}
