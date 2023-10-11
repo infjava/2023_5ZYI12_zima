ddaa6a0e6e864517a8fede4fea2ddf902d0fb4fe
Jan Janech <jan.janech@fri.uniza.sk>
2023-10-11 14:02:52 +0200

    03 - doprogramovana kalkulacka

--- kalkulacka/kalkulacka/Kalkulacka.java
+++ kalkulacka/kalkulacka/Kalkulacka.java
@@ -2,27 +2,27 @@ public class Kalkulacka {
     private double vysledok;
     
     public Kalkulacka() {
-        
+        this.vysledok = 0;
     }
     
     public void zadajCislo(double hodnota) {
-        
+        this.vysledok = hodnota;
     }
     
     public void pripocitaj(double hodnota) {
-        
+        this.vysledok = this.vysledok + hodnota;
     }
     
     public void odpocitaj(double hodnota) {
-        
+        this.vysledok = this.vysledok - hodnota;
     }
     
     public void vynasob(double hodnota) {
-        
+        this.vysledok = this.vysledok * hodnota;
     }
     
     public void vydel(double hodnota) {
-        
+        this.vysledok = this.vysledok / hodnota;
     }
     
     public double getVysledok() {
