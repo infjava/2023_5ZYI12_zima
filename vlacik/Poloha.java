17ae1c929f4a1170b3373b353a2e59039f3d742a
Jan Janech <jan.janech@fri.uniza.sk>
2023-12-06 14:18:33 +0100

    10 - trieda pre polohu cisla vlaku

---
+++ vlacik/Poloha.java
@@ -0,0 +1,31 @@
+public class Poloha {
+    private int x;
+    private int y;
+    private Smer smer;
+    
+    public Poloha(int x, int y, Smer smer) {
+        this.x = x;
+        this.y = y;
+        this.smer = smer;
+    }
+    
+    public int getX() {
+        return this.x;
+    }
+    
+    public int getY() {
+        return this.y;
+    }
+    
+    public Smer getSmer() {
+        return this.smer;
+    }
+    
+    public Poloha getPosunutuPolohu(int krok, Smer smer) {
+        return new Poloha(
+            this.x + smer.getVektorX() * krok,
+            this.y + smer.getVektorY() * krok,
+            smer
+        );
+    }
+}
